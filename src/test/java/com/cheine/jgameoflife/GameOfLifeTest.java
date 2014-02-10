4916f0caff3da14d330e07f8276c9c380d5d2a7b
Conor Heine <conor.heine@gmail.com>
2014-02-09 22:53:16 -0500

    added failing tests for GameOfLife class

---
+++ src/test/java/com/cheine/jgameoflife/GameOfLifeTest.java
@@ -0,0 +1,49 @@
+package com.cheine.jgameoflife;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameOfLifeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTick() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStatusMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopulate() {
+		fail("Not yet implemented");
+	}
+
+}
