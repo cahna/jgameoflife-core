dac3f17b921c9e5f0a42472711813a5b095293d3
Conor Heine <conor.heine@gmail.com>
2014-02-11 18:23:00 -0500

    added comments

--- src/main/java/com/cheine/jgameoflife/Life.java
+++ src/main/java/com/cheine/jgameoflife/Life.java
@@ -1,8 +1,25 @@
 package com.cheine.jgameoflife;
 
-interface Life {	
+interface Life {
+	/**
+	 * @return True if organism is alive (implementation-agnostic), 
+	 * false otherwise.
+	 */
 	public boolean isAlive();
+	
+	/**
+	 * @return True if organism is dead (implementation-agnostic), 
+	 * false otherwise.
+	 */
 	public boolean isDead();
+	
+	/**
+	 * Kill the organism.
+	 */
 	public void kill();
+	
+	/**
+	 * Make the organism living.
+	 */
 	public void ressurect();
 }
