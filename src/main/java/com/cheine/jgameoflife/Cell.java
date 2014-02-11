8a397ef198c990949df0933f16521467ebc7e4bf
Conor Heine <conor.heine@gmail.com>
2014-02-11 18:12:00 -0500

    added Cell class with stubs

---
+++ src/main/java/com/cheine/jgameoflife/Cell.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.cheine.jgameoflife;
+
+/**
+ * @author cheine
+ *
+ */
+public class Cell implements Organism {
+
+	/* (non-Javadoc)
+	 * @see com.cheine.jgameoflife.Organism#isAlive()
+	 */
+	public boolean isAlive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cheine.jgameoflife.Organism#isDead()
+	 */
+	public boolean isDead() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cheine.jgameoflife.Organism#kill()
+	 */
+	public void kill() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cheine.jgameoflife.Organism#ressurect()
+	 */
+	public void ressurect() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
