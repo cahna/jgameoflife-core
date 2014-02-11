2e1c54d6f1ea4f75bdb5051b016de5db67b68157
Conor Heine <conor.heine@gmail.com>
2014-02-11 18:25:40 -0500

    added cell tests

---
+++ src/test/java/com/cheine/jgameoflife/CellTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.cheine.jgameoflife;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author cheine
+ *
+ */
+public class CellTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.cheine.jgameoflife.Cell#Cell(boolean)}.
+	 */
+	@Test
+	public void testCellBoolean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.cheine.jgameoflife.Cell#Cell()}.
+	 */
+	@Test
+	public void testCell() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.cheine.jgameoflife.Cell#isAlive()}.
+	 */
+	@Test
+	public void testIsAlive() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.cheine.jgameoflife.Cell#isDead()}.
+	 */
+	@Test
+	public void testIsDead() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.cheine.jgameoflife.Cell#kill()}.
+	 */
+	@Test
+	public void testKill() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.cheine.jgameoflife.Cell#ressurect()}.
+	 */
+	@Test
+	public void testRessurect() {
+		fail("Not yet implemented");
+	}
+
+}
